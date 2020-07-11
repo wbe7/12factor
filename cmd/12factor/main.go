f1a8a94261f95c95508fedbda29b4c2eb41989e0
Mikhail Tikhonov <wbe7@ya.ru>
2020-07-11 22:35:48 +0300

    Add env variable

--- cmd/12factor/main.go
+++ cmd/12factor/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"github.com/sirupsen/logrus"
+	"os"
 )
 
 func main()  {
 	logrus.Info("Hello world!")
+
+	port := os.Getenv("PORT")
+	if len(port) == 0 {
+		logrus.Fatal("Port is not set")
+	}
 }
