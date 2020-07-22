02d5caa384e4e6bb6fbc156ad9653a74fc5ebf4c
Mikhail Tikhonov <wbe7@ya.ru>
2020-07-23 02:08:30 +0300

    Graceful shutdown

--- cmd/12factor/main.go
+++ cmd/12factor/main.go
@@ -1,15 +1,45 @@
 package main
 
 import (
-	"github.com/sirupsen/logrus"
+	"context"
+	"net"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/sirupsen/logrus"
+	"github.com/gorilla/mux"
 )
 
 func main()  {
 	logrus.Info("Hello world!")
 
 	port := os.Getenv("PORT")
-	if len(port) == 0 {
+	if port == "" {
 		logrus.Fatal("Port is not set")
 	}
+
+	router := mux.NewRouter()
+	router.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		logrus.Infof("Receive request from %v", r.RemoteAddr)
+	})
+
+	serv := http.Server{
+		Addr: net.JoinHostPort("", port),
+		Handler: router,
+	}
+
+	go serv.ListenAndServe()
+
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	<-interrupt
+
+	timeout, cancelFunc := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancelFunc()
+	serv.Shutdown(timeout)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/wbe7/12factor
 
 go 1.14
 
-require github.com/sirupsen/logrus v1.6.0
+require (
+	github.com/gorilla/mux v1.7.4
+	github.com/sirupsen/logrus v1.6.0
+)
